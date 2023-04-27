a76beed16699b32f0481da69d7cdee40187caf4d
RaulMLA <raulmanzanerolopezaguado@gmail.com>
2023-04-27 20:12:56 +0200

    Base construcción de modelos

--- all.py
+++ all.py
@@ -12,7 +12,7 @@ df = pd.DataFrame(data)
 print(df)
 
 # Mostramos la información del conjunto de datos.
-print(f"El conjunto de datos tiene {len(df)} instancias.")
+print(f'El conjunto de datos tiene {len(df)} instancias.')
 
 # Datos.
 X = df.drop('Attrition', axis=1)
@@ -62,26 +62,26 @@ y.describe()
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
-# Realizamos una división train/test "normal".
+# Realizamos una división train/test 'normal'.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=2/10, random_state=13, shuffle=True)
 
 # Codificamos las etiquetas de las clases
 le = LabelEncoder()
 y_encoded = le.fit_transform(y)
-y_train_encoded = le.fit_transform(y_train)
-y_test_encoded = le.transform(y_test)
+y_train = le.fit_transform(y_train)
+y_test = le.transform(y_test)
 
 N_SAMPLES_NEG = np.sum(y_encoded == 0)
 N_SAMPLES_POS = np.sum(y_encoded == 1)
 
 # Ahora comprobamos la distribución de las clases.
 n_pos_pct = N_SAMPLES_POS / (N_SAMPLES_POS + N_SAMPLES_NEG)
-n_pos_train_pct = np.sum(y_train_encoded) / len(y_train_encoded)
-n_pos_test_pct = np.sum(y_test_encoded) / len(y_test_encoded)
+n_pos_train_pct = np.sum(y_train) / len(y_train)
+n_pos_test_pct = np.sum(y_test) / len(y_test)
 
-print(f"Hay un {100 * n_pos_train_pct:.1f} % de instancias positivas en train.")
-print(f"Hay un {100 * n_pos_test_pct:.1f} % de instancias positivas en test.")
-print(f"Hay un {100 * n_pos_pct:.1f} % de instancias positivas en total.")
+print(f'Hay un {100 * n_pos_train_pct:.1f} % de instancias positivas en train.')
+print(f'Hay un {100 * n_pos_test_pct:.1f} % de instancias positivas en test.')
+print(f'Hay un {100 * n_pos_pct:.1f} % de instancias positivas en total.')
 
 
 # Ahora realizaremos una división train/test estratificada.
@@ -90,20 +90,20 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=2/10, random
 # Codificamos las etiquetas de las clases
 le = LabelEncoder()
 y_encoded = le.fit_transform(y)
-y_train_encoded = le.fit_transform(y_train)
-y_test_encoded = le.transform(y_test)
+y_train = le.fit_transform(y_train)
+y_test= le.transform(y_test)
 
 N_SAMPLES_NEG = np.sum(y_encoded == 0)
 N_SAMPLES_POS = np.sum(y_encoded == 1)
 
 # Ahora comprobamos la distribución de las clases.
 n_pos_pct = N_SAMPLES_POS / (N_SAMPLES_POS + N_SAMPLES_NEG)
-n_pos_train_pct = np.sum(y_train_encoded) / len(y_train_encoded)
-n_pos_test_pct = np.sum(y_test_encoded) / len(y_test_encoded)
+n_pos_train_pct = np.sum(y_train) / len(y_train)
+n_pos_test_pct = np.sum(y_test) / len(y_test)
 
-print(f"Hay un {100 * n_pos_train_pct:.1f} % de instancias positivas en train.")
-print(f"Hay un {100 * n_pos_test_pct:.1f} % de instancias positivas en test.")
-print(f"Hay un {100 * n_pos_pct:.1f} % de instancias positivas en total.")
+print(f'Hay un {100 * n_pos_train_pct:.1f} % de instancias positivas en train.')
+print(f'Hay un {100 * n_pos_test_pct:.1f} % de instancias positivas en test.')
+print(f'Hay un {100 * n_pos_pct:.1f} % de instancias positivas en total.')
 
 
 
@@ -119,14 +119,14 @@ cols_categoricas = X.select_dtypes(include=['object', 'category']).columns.tolis
 # Obtenemos los nombres de las columnas que tienen tipo de dato 'int' o 'float'.
 cols_numericas = X.select_dtypes(include=['int', 'float']).columns.tolist()
 
-print("Columnas categóricas: ", cols_categoricas)
-print("Columnas numéricas: ", cols_numericas)
+print('Columnas categóricas: ', cols_categoricas)
+print('Columnas numéricas: ', cols_numericas)
 
 # Procesamos valores numéricos.
 imputer_num = SimpleImputer(strategy='mean')
 pipeline_num = Pipeline(
     steps=[
-        ("imputer", imputer_num)
+        ('imputer', imputer_num)
     ]
 )
 
@@ -135,17 +135,17 @@ imputer_cat = SimpleImputer(strategy='most_frequent')
 encoder_cat = OneHotEncoder()
 pipeline_cat = Pipeline(
     steps=[
-        ("imputer", imputer_cat),
-        ("onehot", encoder_cat)
+        ('imputer', imputer_cat),
+        ('onehot', encoder_cat)
     ]
 )
 
-# Pre-procesador "global".
+# Pre-procesador 'global'.
 # Dependiendo del tipo de columna aplicamos una transformación u otra.
 processor = ColumnTransformer(
     transformers=[
-        ("num", pipeline_num, cols_numericas),
-        ("cat", pipeline_cat, cols_categoricas),
+        ('num', pipeline_num, cols_numericas),
+        ('cat', pipeline_cat, cols_categoricas),
     ]
 )
 
@@ -167,7 +167,7 @@ from sklearn.linear_model import LogisticRegression
 model = LogisticRegression(random_state=13, solver='liblinear')
 
 # Lo entrenamos.
-model.fit(X_train, y_train_encoded)
+model.fit(X_train, y_train)
 
 # Predecimos sobre el conjunto de test.
 y_pred = model.predict(X_test)
@@ -176,11 +176,209 @@ y_pred = model.predict(X_test)
 from sklearn.metrics import balanced_accuracy_score, f1_score, confusion_matrix
 
 # Calculamos las métricas.
-balanced_accuracy = balanced_accuracy_score(y_test_encoded, y_pred)
-f1 = f1_score(y_test_encoded, y_pred)
-confusion_matrix = confusion_matrix(y_test_encoded, y_pred)
+balanced_accuracy = balanced_accuracy_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+confusion_matrix = confusion_matrix(y_test, y_pred)
 
-print(f"Balanced accuracy: {balanced_accuracy:.4f}")
-print(f"F1: {f1:.4f}")
-print(f"Confusion matrix:\n{confusion_matrix}")
+print(f'Balanced accuracy: {balanced_accuracy:.4f}')
+print(f'F1: {f1:.4f}')
+print(f'Confusion matrix:\n{confusion_matrix}')
 
+
+
+
+
+# Importamos todas las implementaciones de boosting que vamos a probar.
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, HistGradientBoostingClassifier
+
+# Librerias externas (extra).
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+
+# Importamos time.
+import time
+
+# Creamos un diccionario con los modelos que vamos a evaluar.
+modelos = {
+    'AdaBoost': AdaBoostClassifier(random_state=13),
+    'Gradient Boosting': GradientBoostingClassifier(random_state=13),
+    'XGBoost': XGBClassifier(random_state=13),
+    'LightGBM': LGBMClassifier(random_state=13, class_weight='balanced'),
+}
+
+
+# Creamos una función para evaluar los modelos.
+def evaluar_modelos(models, X_train, y_train, X_test, y_test):
+    # Creamos un diccionario para guardar los resultados.
+    resultados = {}
+
+    # Iteramos sobre los modelos.
+    for nombre_modelo, modelo in models.items():
+        print(f'Evaluando modelo: {nombre_modelo}')
+
+        # Entrenamos el modelo.
+        start = time.time()
+        modelo.fit(X_train, y_train)
+        end = time.time()
+        tiempo = end - start
+
+        # Predecimos sobre el conjunto de test.
+        y_pred = modelo.predict(X_test)
+
+        # Calculamos las métricas.
+        balanced_accuracy = balanced_accuracy_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+        #conf_matrix = confusion_matrix(y_test, y_pred)
+
+        # Guardamos los resultados.
+        resultados[nombre_modelo] = {
+            'tiempo': tiempo,
+            'model': modelo,
+            'balanced_accuracy': balanced_accuracy,
+            'f1': f1
+            #'confusion_matrix': conf_matrix
+        }
+
+    return resultados
+
+# Evaluamos los modelos.
+resultados = evaluar_modelos(modelos, X_train, y_train, X_test, y_test)
+
+# Mostramos los resultados.
+for nombre_modelo, resultado in resultados.items():
+    print(f'\nModelo: {nombre_modelo}')
+    print(f'Tiempo de ejecución: {resultado["tiempo"]:.5f} segundos')
+    print(f'Balanced accuracy: {resultado["balanced_accuracy"]:.4f}')
+    print(f'F1: {resultado["f1"]:.4f}')
+    #print(f'Confusion matrix:\n{resultado['confusion_matrix']}')
+
+
+
+
+
+# Importamos la clase StratifiedKFold para realizar validación cruzada estratificada.
+from sklearn.model_selection import StratifiedKFold
+
+# Importamos GridSearchCV para realizar búsqueda de hiper-parámetros.
+from sklearn.model_selection import GridSearchCV
+
+# StandardScaler para escalar los datos.
+from sklearn.preprocessing import StandardScaler
+
+cv = StratifiedKFold(n_splits=5)
+
+# Creamos un diccionario con los parámetros que vamos a probar.
+parametros = {
+    'AdaBoost': {
+        'model__n_estimators': [50, 100, 200],
+        'model__learning_rate': [0.1, 0.5, 1.0],
+    },
+    'Gradient Boosting': {
+        'model__n_estimators': [50, 100, 200],
+        'model__learning_rate': [0.1, 0.5, 1.0],
+        'model__max_depth': [3, 5, 7],
+    },
+    'XGBoost': {
+        'model__n_estimators': [50, 100, 200],
+        'model__learning_rate': [0.1, 0.5, 1.0],
+        'model__max_depth': [3, 5, 7],
+    },
+    'LightGBM': {
+        'model__n_estimators': [50, 100, 200],
+        'model__learning_rate': [0.1, 0.5, 1.0],
+        'model__max_depth': [3, 5, 7],
+    }
+}
+
+# Creamos una función para evaluar y ajustar los modelos.
+def ajustar_evaluar_modelos(modelos, parametros, X_train, y_train, X_test, y_test, cv):
+    # Creamos un diccionario para guardar los resultados.
+    resultados = {}
+
+    # Iteramos sobre los modelos.
+    for nombre_modelo, modelo in modelos.items():
+        print(f'Evaluando modelo: {nombre_modelo}')
+
+        # Definimos el pipeline.
+        pipeline = Pipeline([
+            ('scaler', StandardScaler()),
+            ('model', modelo)
+        ])
+
+        # Definimos la búsqueda por grid search con validación cruzada.
+        grid = GridSearchCV(
+            pipeline,
+            parametros[nombre_modelo],
+            cv=cv,
+            scoring='balanced_accuracy',
+            n_jobs=-1,
+            verbose=1
+        )
+
+        # Entrenamos el modelo.
+        start = time.time()
+        grid.fit(X_train, y_train)
+        end = time.time()
+        tiempo = end - start
+        
+        
+
+        # Predecimos sobre el conjunto de test.
+        y_pred = grid.predict(X_test)
+
+        # Calculamos las métricas.
+        balanced_accuracy = balanced_accuracy_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+        #conf_matrix = confusion_matrix(y_test, y_pred)
+
+        # Guardamos los resultados.
+        resultados[nombre_modelo] = {
+            'tiempo': tiempo,
+            'model': grid.best_estimator_,
+            'balanced_accuracy': balanced_accuracy,
+            'f1': f1
+            #'confusion_matrix': conf_matrix
+        }
+
+    return resultados
+
+# Evaluamos los modelos con ajuste de hiperparámetros.
+resultados_ajuste = ajustar_evaluar_modelos(modelos, parametros, X_train, y_train, X_test, y_test, cv)
+
+# Mostramos los resultados.
+for nombre_modelo, resultado in resultados_ajuste.items():
+    print(f'\nModelo: {nombre_modelo}')
+    print(f'Tiempo de ejecución: {resultado["tiempo"]:.5f} segundos')
+    print(f'Balanced accuracy: {resultado["balanced_accuracy"]:.4f}')
+    print(f'F1: {resultado["f1"]:.4f}')
+    #print(f'Confusion matrix:\n{resultado['confusion_matrix']}')
+
+
+
+
+
+# Importamos f_classif, mutual_info_classif y chi2.
+from sklearn.feature_selection import f_classif, mutual_info_classif, chi2
+
+# Importamos SelectKBest.
+from sklearn.feature_selection import SelectKBest
+
+# Creamos un selector de características usando f_classif.
+selector = SelectKBest(score_func=f_classif, k=10)
+
+# Aplicamos el selector sobre los datos de entrenamiento y prueba.
+X_train_sel = selector.fit_transform(X_train, y_train)
+X_test_sel = selector.transform(X_test)
+
+# Creamos un modelo de Regresión Logística y lo entrenamos con las características seleccionadas.
+modelo = LogisticRegression()
+
+start = time.time()
+modelo.fit(X_train_sel, y_train)
+end = time.time()
+tiempo = end - start
+
+# Evaluamos el modelo con las características seleccionadas.
+score = modelo.score(X_test_sel, y_test)
+print(f'Accuracy con las características seleccionadas: {score}')
+print(f'Tiempo de ejecución: {tiempo:.5f} segundos')
